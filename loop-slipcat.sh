0b9d591b5ff366ad93d6af4b38589e740f739f85
Oleg Zhurakivskyy <oleg.zhurakivskyy@intel.com>
2019-03-27 14:10:47 +0200

    loop-slipcat.sh: Update copyright

--- loop-slipcat.sh
+++ loop-slipcat.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright © 2018, Intel Corporation.
+# Copyright © 2018-2019, Intel Corporation.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms and conditions of the GNU Lesser General Public License,
