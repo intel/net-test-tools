9bd5f502837f3db50a99011001e69c928d1c3b8d
Oleg Zhurakivskyy <oleg.zhurakivskyy@intel.com>
2019-04-03 20:38:00 +0300

    slipcat: Add s_in_new()

--- slipcat.c
+++ slipcat.c
@@ -12,6 +12,8 @@
  *
  */
 
+#define _GNU_SOURCE
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
@@ -693,6 +695,50 @@ static void options_parse(int *argc, char **argv[])
 		opt_af_unix = 0;
 		opt_slip = 0;
 	}
+}
+
+/*
+  Input:
+    - ipv4:port
+    - ipv4
+    - :port
+ */
+struct sockaddr_in *s_in_new(const char *s)
+{
+	struct sockaddr_in *s_in = calloc(1, sizeof(*s_in));
+	int r, addr_len = strlen(s);
+	const char *addr, *sp = strchr(s, ':');
+
+	if (sp) {
+		int port = atoi(sp + 1);
+
+		if (port > 0 && port <= 65535) {
+			s_in->sin_port = htons(port);
+		}
+
+		addr_len = sp - s;
+	}
+
+	if (addr_len) {
+		addr = (const char *) strndupa(s, addr_len);
+
+		r = inet_pton(AF_INET, addr, &s_in->sin_addr);
+
+		if (r <= 0) {
+			if (r == 0)
+				fprintf(stderr, "Not in presentation format");
+			else
+				perror("inet_pton");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	D("in: '%s', out: %s:%hu", s,
+		inet_ntoa(s_in->sin_addr), ntohs(s_in->sin_port));
+
+	s_in->sin_family = AF_INET;
+
+	return s_in;
 }
 
 int main(int argc, char *argv[])
